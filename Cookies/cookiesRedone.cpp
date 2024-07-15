8a4974bc896c2e715a5aeafba7c0c5065e67a716
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-15 18:29:54 +1000

    Basically solved Cookies
    Wrote a more straightforward greedy algorithm that covers all bases. Still failing about three testcases though.

--- Cookies/cookiesRedone.cpp
+++ Cookies/cookiesRedone.cpp
@@ -7,32 +7,114 @@ int main() {
     int D, C0, P1, C1, P2, C2;
     cin >> D >> C0 >> P1 >> C1 >> P2 >> C2;
 
+    int one, two, three, four, five;
+
     int total = 0;
     int production = C0;
     int bought1 = false;
     int bought2 = false;
 
-    int daysLeft;
+    // none bought
     for (int i = 1; i < D+1; i++) {
-        daysLeft = D - i;
         total += production;
+    }
 
-        int case1 = total - P1 + (C1*daysLeft);
-        int case2 = total - P2 + (C2*daysLeft);
+    one = total;
 
-        if (C1 == C2) { // if equal
-            if (C1 != 0) {
-                if (case1 > case2 && case1 > total) {
-                    if (case)
-                }
-                // if both are not 0
-            }
-        } else if (C1 > C2) {
+    total = 0;
+    production = C0;
+    bought1 = false;
+    bought2 = false;
 
-        } else if (C2 > C1) {
+    // both bought
+    for (int i = 1; i < D+1; i++) {
+        total += production;
+        
+        if (bought1 == false && total >= P1) {
+            bought1 = true;
+            production += C1;
+            total -= P1;
+        }
 
+        if (bought2 == false && total >= P2) {
+            bought2 = true;
+            production += C2;
+            total -= P2;
         }
     }
-    cout << total;
+
+    two = total;
+
+    total = 0;
+    production = C0;
+    bought1 = false;
+    bought2 = false;
+
+    for (int i = 1; i < D+1; i++) {
+        total += production;
+
+        if (bought2 == false && total >= P2) {
+            bought2 = true;
+            production += C2;
+            total -= P2;
+        }
+
+        if (bought1 == false && total >= P1) {
+            bought1 = true;
+            production += C1;
+            total -= P1;
+        }
+    }
+
+    three = total;
+
+    total = 0;
+    production = C0;
+    bought1 = false;
+    bought2 = false;
+
+    // only factory one bought
+    for (int i = 1; i < D+1; i++) {
+        total += production;
+
+        if (bought1 == false && total >= P1) {
+            bought1 = true;
+            production += C1;
+            total -= P1;
+        }
+    }
+
+    four = total;
+
+    total = 0;
+    production = C0;
+    bought1 = false;
+    bought2 = false;
+
+    // only factory two bought
+    for (int i = 1; i < D+1; i++) {
+        total += production;
+
+        if (bought2 == false && total >= P2) {
+            bought2 = true;
+            production += C2;
+            total -= P2;
+        }
+    }
+
+    five = total;
+
+    // cout << one << endl;
+    // cout << two << endl;
+    // cout << three << endl;
+    // cout << four << endl;
+    // cout << five << endl;
+
+    int answer = max(
+        max(one, two),
+        max(max(three, four), five)
+    );
+
+    cout << answer;
     return 0;
 }
